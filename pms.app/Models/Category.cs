fbec245390b1d4aff788a3ce7f17cb5f12dc8b24
Pedro Santana <pedrosantanait@gmail.com>
2024-04-09 10:35:55 -0400

    Added category CRUD pages

--- pms.app/Models/Category.cs
+++ pms.app/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace pms.app.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pms.app.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Category Name is required.")]
         public string Name { get; set; }
         public string? Description { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
